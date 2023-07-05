d35641ac6cf5dc78ed350e4216072cbbf7b2455e
Saintmuchacho <itsmobid@gmail.com>
2023-07-06 00:04:28 +0300

    installing packages

--- src/Message.tsx
+++ src/Message.tsx
@@ -1,6 +1,9 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 
 const Message = () => {
+  useEffect(()=>{
+    document.title='message';
+  });
   return (
     <div>shoot for perfections</div>
   )
