f30adac9678394caaedea486b1fb03677a6d568f
Saintmuchacho <itsmobid@gmail.com>
2023-07-06 13:08:54 +0300

    fetching Data

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,36 @@
-import { useEffect} from "react";
-
-const App = () => {
-  const connect = () => console.log("connecting");
-  const disconnect = () => console.log("disconnecting");
-  // effectHook clean up function
+import axios from "axios";
+import { useEffect, useState } from "react";
+// hapa na define interface iliniweze kuaccess
+// vizuri user object ya kwa fake backend ya
+// json
+interface Users {
+  //hii ni kwa ajiri ya autoComplition and
+  // type safety
+  id: number;
+  name: string;
+  email: string;
+}
+const App = ({}) => {
+  // weka useState kwa ajiri ya kustore our users
+  const [users, setUsers] = useState<Users[]>([]);
+  // effectHook fetching data from backend
   useEffect(() => {
-    // setup code
-    connect();
-    // clean up code
-    return () => disconnect();
-  });
-  return <div></div>;
+    // natumia axios kwa ajiri ya kusend request
+    // kwenye server
+    axios
+      .get<Users[]>("https://jsonplaceholder.typicode.com/users")
+      .then((res) => setUsers(res.data)); //a promise
+  }, []);
+  return (
+    <div>
+      <ul>
+        {users.map((user) => (
+          <li key={user.id}>{user.email}</li>
+          // user.(either email/name)
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 export default App;
