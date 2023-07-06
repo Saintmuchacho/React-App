aa240c0c42cff665a815aa4d1ea0f61f02f21638
Saintmuchacho <itsmobid@gmail.com>
2023-07-06 12:07:29 +0300

    useEffect

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,20 @@
-import React from 'react'
-import Message from './Message'
+import { useEffect, useRef } from "react";
 
 const App = () => {
+  // hapa unaanza kudefine useRef yako iliuweze kuweka focus kwenye
+  // input field yako
+  const ref = useRef<HTMLInputElement>(null);
+  // hapa unaweka effectHook yako
+  useEffect(() => {
+    // sideEffect
+    if (ref.current) ref.current.focus();
+  });
   return (
     <div>
-        <Message/>
+      {/* nini maana ya effectHook */} 
+      <input ref={ref} type="text" className="form-control" />
     </div>
-  )
-}
-
+  );
+};
+// kazi ya effectHook ni kukeep componet pure
 export default App;
